eced99652abbbebf6930e6f2627e3fafc33be935
Cliff Rogers <emptyresearch@outlook.com>
2017-08-17 08:52:56 -0400

    Create SendMail.php

---
+++ Submit-Order/SendMail.php
@@ -0,0 +1,8 @@
+ <html>
+ <body>
+ <?php 
+   $text= "Username: $Username <br> Item Request: $ItemRequest <br> Description: $Description <br> Order Time: $OrderTime <br> Additional Information: $AdditionalInfo <br><br><br>";
+   mail ('emptyresearch@outlook.com', 'Arcadia Modern Order', $text)?>
+
+</body>
+</html>
